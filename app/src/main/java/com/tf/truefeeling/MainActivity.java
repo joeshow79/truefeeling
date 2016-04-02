3fcf60c145fa577e8893a41b845d3c8f1266b276
jasonj <35540346@qq.com>
2016-04-02 16:18:30 +0800

    Init release could run correctly.

---
+++ app/src/main/java/com/tf/truefeeling/MainActivity.java
@@ -0,0 +1,134 @@
+package com.tf.truefeeling;
+
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView toolbar = (TextView) findViewById(R.id.toolbar);
+        toolbar.setText(R.string.main_activity_title);
+        //2016-04-2 jj: Replace action bar with
+        /*setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });*/
+
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        SampleFragmentPagerAdapter pagerAdapter =
+                new SampleFragmentPagerAdapter(getSupportFragmentManager(), this);
+
+        viewPager.setAdapter(pagerAdapter);
+
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            if (tab != null) {
+                tab.setCustomView(pagerAdapter.getTabView(i));
+            }
+        }
+
+        viewPager.setCurrentItem(1);
+
+    }
+
+
+    public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+        final int PAGE_COUNT = 3;
+
+        private Context context;
+        private String tabTitles[] = new String[]{getApplicationContext().getResources().getString(R.string.tab_connection),getApplicationContext().getResources().getString(R.string.tab_status),getApplicationContext().getResources().getString(R.string.tab_profile)};
+        private int drawId[]=new int[]{R.drawable.main_tab_connection,R.drawable.main_tab_status,R.drawable.main_tab_profile};
+
+        public View getTabView(int position) {
+            View v = LayoutInflater.from(context).inflate(R.layout.custom_tab, null);
+            TextView tv = (TextView) v.findViewById(R.id.textView);
+            tv.setText(tabTitles[position]);
+            ImageView img = (ImageView) v.findViewById(R.id.imageView);
+            //TODO
+            //img.setImageResource(imageResId[position]);
+            img.setImageResource(drawId[position]);
+            return v;
+        }
+
+        public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+    }
+
+    public static class PageFragment extends Fragment {
+        public static final String ARG_PAGE = "ARG_PAGE";
+
+        private int mPage;
+
+        public static PageFragment newInstance(int page) {
+            Bundle args = new Bundle();
+            args.putInt(ARG_PAGE, page);
+            PageFragment fragment = new PageFragment();
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mPage = getArguments().getInt(ARG_PAGE);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_page, container, false);
+            TextView textView = (TextView) view;
+            textView.setText("Fragment #" + mPage);
+            return view;
+        }
+    }
+
+}
+
---
+++ app/src/main/java/com/tf/truefeeling/TFApplication.java
@@ -0,0 +1,21 @@
+package com.tf.truefeeling;
+
+import android.app.Application;
+
+import com.avos.avoscloud.AVOSCloud;
+/**
+ * Created by admin on 2016/4/1.
+ */
+public class TFApplication extends Application{
+
+        @Override
+        public void onCreate()
+        {
+            super.onCreate();
+            setupLeanCloud(); // 初始化全局变量
+        }
+
+        private void setupLeanCloud() {
+            AVOSCloud.initialize(this, "7K8Rg1kJkFjftzHtmCsPO4LP-gzGzoHsz","P952rbIbhSRmUdHnW18Mt8kx");
+        }
+}
