f09f984e22a0ee3c1a24434c2f3a782941f75cf3
jasonj <35540346@qq.com>
2016-04-12 19:31:44 +0800

    Remove gabage code; Add log utility.

---
+++ app/src/main/java/com/tf/truefeeling/Util/Log.java
@@ -0,0 +1,287 @@
+package com.tf.truefeeling.Util;
+
+/**
+ * Created by admin on 2016/4/12.
+ */
+public class Log {
+    public static final int LEVEL_VERBOSE = 0;
+    public static final int LEVEL_DEBUG = 1;
+    public static final int LEVEL_INFO = 2;
+    public static final int LEVEL_WARNING = 3;
+    public static final int LEVEL_ERROR = 4;
+    public static final int LEVEL_FATAL = 5;
+
+    private static int sLevel = LEVEL_VERBOSE;
+
+    /**
+     * set log level, the level lower than this level will not be logged
+     *
+     * @param level
+     */
+    public static void setLogLevel(int level) {
+        sLevel = level;
+    }
+
+    /**
+     * Send a VERBOSE log message.
+     *
+     * @param tag
+     * @param msg
+     */
+    public static void v(String tag, String msg) {
+        if (sLevel > LEVEL_VERBOSE) {
+            return;
+        }
+        android.util.Log.v(tag, msg);
+    }
+
+    /**
+     * Send a VERBOSE log message.
+     *
+     * @param tag
+     * @param msg
+     * @param throwable
+     */
+    public static void v(String tag, String msg, Throwable throwable) {
+        if (sLevel > LEVEL_VERBOSE) {
+            return;
+        }
+        android.util.Log.v(tag, msg, throwable);
+    }
+
+    /**
+     * Send a VERBOSE log message.
+     *
+     * @param tag
+     * @param msg
+     * @param args
+     */
+    public static void v(String tag, String msg, Object... args) {
+        if (sLevel > LEVEL_VERBOSE) {
+            return;
+        }
+        if (args.length > 0) {
+            msg = String.format(msg, args);
+        }
+        android.util.Log.v(tag, msg);
+    }
+
+    /**
+     * Send a DEBUG log message
+     *
+     * @param tag
+     * @param msg
+     */
+    public static void d(String tag, String msg) {
+        if (sLevel > LEVEL_DEBUG) {
+            return;
+        }
+        android.util.Log.d(tag, msg);
+    }
+
+    /**
+     * Send a DEBUG log message
+     *
+     * @param tag
+     * @param msg
+     * @param args
+     */
+    public static void d(String tag, String msg, Object... args) {
+        if (sLevel > LEVEL_DEBUG) {
+            return;
+        }
+        if (args.length > 0) {
+            msg = String.format(msg, args);
+        }
+        android.util.Log.d(tag, msg);
+    }
+
+    /**
+     * Send a DEBUG log message
+     *
+     * @param tag
+     * @param msg
+     * @param throwable
+     */
+    public static void d(String tag, String msg, Throwable throwable) {
+        if (sLevel > LEVEL_DEBUG) {
+            return;
+        }
+        android.util.Log.d(tag, msg, throwable);
+    }
+
+    /**
+     * Send an INFO log message
+     *
+     * @param tag
+     * @param msg
+     */
+    public static void i(String tag, String msg) {
+        if (sLevel > LEVEL_INFO) {
+            return;
+        }
+        android.util.Log.i(tag, msg);
+    }
+
+    /**
+     * Send an INFO log message
+     *
+     * @param tag
+     * @param msg
+     * @param args
+     */
+    public static void i(String tag, String msg, Object... args) {
+        if (sLevel > LEVEL_INFO) {
+            return;
+        }
+        if (args.length > 0) {
+            msg = String.format(msg, args);
+        }
+        android.util.Log.i(tag, msg);
+    }
+
+    /**
+     * Send an INFO log message
+     *
+     * @param tag
+     * @param msg
+     */
+    public static void i(String tag, String msg, Throwable throwable) {
+        if (sLevel > LEVEL_INFO) {
+            return;
+        }
+        android.util.Log.i(tag, msg, throwable);
+    }
+
+    /**
+     * Send a WARNING log message
+     *
+     * @param tag
+     * @param msg
+     */
+    public static void w(String tag, String msg) {
+        if (sLevel > LEVEL_WARNING) {
+            return;
+        }
+        android.util.Log.w(tag, msg);
+    }
+
+    /**
+     * Send a WARNING log message
+     *
+     * @param tag
+     * @param msg
+     * @param args
+     */
+    public static void w(String tag, String msg, Object... args) {
+        if (sLevel > LEVEL_WARNING) {
+            return;
+        }
+        if (args.length > 0) {
+            msg = String.format(msg, args);
+        }
+        android.util.Log.w(tag, msg);
+    }
+
+    /**
+     * Send a WARNING log message
+     *
+     * @param tag
+     * @param msg
+     * @param throwable
+     */
+    public static void w(String tag, String msg, Throwable throwable) {
+        if (sLevel > LEVEL_WARNING) {
+            return;
+        }
+        android.util.Log.w(tag, msg, throwable);
+    }
+
+    /**
+     * Send an ERROR log message
+     *
+     * @param tag
+     * @param msg
+     */
+    public static void e(String tag, String msg) {
+        if (sLevel > LEVEL_ERROR) {
+            return;
+        }
+        android.util.Log.e(tag, msg);
+    }
+
+    /**
+     * Send an ERROR log message
+     *
+     * @param tag
+     * @param msg
+     * @param args
+     */
+    public static void e(String tag, String msg, Object... args) {
+        if (sLevel > LEVEL_ERROR) {
+            return;
+        }
+        if (args.length > 0) {
+            msg = String.format(msg, args);
+        }
+        android.util.Log.e(tag, msg);
+    }
+
+    /**
+     * Send an ERROR log message
+     *
+     * @param tag
+     * @param msg
+     * @param throwable
+     */
+    public static void e(String tag, String msg, Throwable throwable) {
+        if (sLevel > LEVEL_ERROR) {
+            return;
+        }
+        android.util.Log.e(tag, msg, throwable);
+    }
+
+    /**
+     * Send a FATAL ERROR log message
+     *
+     * @param tag
+     * @param msg
+     */
+    public static void f(String tag, String msg) {
+        if (sLevel > LEVEL_FATAL) {
+            return;
+        }
+        android.util.Log.wtf(tag, msg);
+    }
+
+    /**
+     * Send a FATAL ERROR log message
+     *
+     * @param tag
+     * @param msg
+     * @param args
+     */
+    public static void f(String tag, String msg, Object... args) {
+        if (sLevel > LEVEL_FATAL) {
+            return;
+        }
+        if (args.length > 0) {
+            msg = String.format(msg, args);
+        }
+        android.util.Log.wtf(tag, msg);
+    }
+
+    /**
+     * Send a FATAL ERROR log message
+     *
+     * @param tag
+     * @param msg
+     * @param throwable
+     */
+    public static void f(String tag, String msg, Throwable throwable) {
+        if (sLevel > LEVEL_FATAL) {
+            return;
+        }
+        android.util.Log.wtf(tag, msg, throwable);
+    }
+}
